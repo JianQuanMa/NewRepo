51f0748131cebd47b98228d9695d566e60d76d3c
Jian Ma <jianquanma@gmail.com>
2020-03-19 09:35:56 -0400

    Initial Commit

---
+++ NewRepo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewRepo
+//
+//  Created by Jian Ma on 3/19/20.
+//  Copyright © 2020 Jian Ma. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
